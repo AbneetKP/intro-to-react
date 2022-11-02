cfc6ba6fc7dd1e6567c6a389c93fb2ff174b5b12
Abneet K Pabyal <Abneet.P@gmail.com>
2022-11-02 11:27:00 -0600

    added buttons and removed logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+  function increaseCount(amount) {
+    setCount(count + amount)
+  }
   return (
     <div className="App">
-      <header className="App-header"> </header>
+      <header className="App-header"> The count is {count}
+      <button onClick = {() => increaseCount(1)}> Increase Count </button>
+      <button onClick = {() => increaseCount(2)}> Increase Count By Two </button>
+
+       </header>
     </div>
   );
 }
